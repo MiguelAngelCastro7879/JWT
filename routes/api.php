afa9bc762e87127f8b195a520183681dcd3f4dc6
MiguelAngelCastro7879 <mikeangel7879@gmail.com>
2021-12-05 16:14:18 -0600

    Se crean rutas para pedir los datos del sensor de tempepratura-humedad, ultrasonico, y del servomotor

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PuertaController;
+use App\Http\Controllers\DistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +23,9 @@ use App\Http\Controllers\AuthController;
 
 $response = Http::get('http://example.com');
 
-Route::get('a', function(){
+Route::get('/a', function(){
 
-    
+    return "<h1>hola</h1>";
 });
 // estas rutas se pueden acceder sin proveer de un token válido.
 Route::post('/login', [AuthController::class, 'login']);
@@ -31,4 +33,22 @@ Route::post('/register', [AuthController::class, 'register']);
 // estas rutas requiren de un token válido para poder accederse.
 Route::group(['middleware' => 'auth.jwt'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::prefix('puerta')->group(function () {
+    Route::get( '/datos', [PuertaController::class, 'datos']);
+    Route::get( '/last', [PuertaController::class, 'ultimoDato']);
+    Route::get( '/controller', [PuertaController::class, 'controlador']);
+});
+
+Route::prefix('distancia')->group(function () {
+    Route::get( '/datos', [DistController::class, 'datos']);
+    Route::get( '/last', [DistController::class, 'ultimoDato']);
+});
+Route::prefix('dht')->group(function () {
+    Route::get( 'temperatura/datos', [PuertaController::class, 'datosPuerta']);
+    Route::get( 'temperatura/last', [PuertaController::class, 'datosPuerta']);
+    Route::get( 'humedad/datos', [PuertaController::class, 'datosPuerta']);
+    Route::get( 'humedad/last', [PuertaController::class, 'datosPuerta']);
 });
